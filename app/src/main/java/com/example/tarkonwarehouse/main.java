20d8e3d820afea1f527488b344d9bb162a4f81c8
RafixOOO <smoktbg@gmail.com>
2023-08-07 12:32:55 +0200

    1.0

--- app/src/main/java/com/example/tarkonwarehouse/main.java
+++ app/src/main/java/com/example/tarkonwarehouse/main.java
@@ -1,8 +1,13 @@
 package com.example.tarkonwarehouse;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 
 public class main extends AppCompatActivity {
@@ -13,7 +18,26 @@ public class main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.change) {
+            // Obsługa akcji zmiany
+            return true;
+        } else if (item.getItemId() == R.id.logout) {
+            // Obsługa akcji wylogowania
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
