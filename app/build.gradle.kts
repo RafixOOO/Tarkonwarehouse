68f7223a40667804ca4f155479c4be90fda3af27
Rafał Pezda <smoktbg@gmail.com>
2024-01-16 15:31:34 +0100

    1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(files("libs\\jtds-1.3.1.jar"))
+    implementation("com.google.firebase:firebase-inappmessaging:20.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
