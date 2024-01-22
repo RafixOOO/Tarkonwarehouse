ddbe34a61aed7bda98821e163121bcac894585ec
Rafał Pezda <smoktbg@gmail.com>
2024-01-22 14:42:15 +0100

    1.0

---
+++ app/src/main/java/com/example/tarkonwarehouse/ustallokalizacje.java
@@ -0,0 +1,73 @@
+package com.example.tarkonwarehouse;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class ustallokalizacje extends AppCompatActivity {
+
+    private String jwtToken;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ustallokalizacje);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        jwtToken = getIntent().getStringExtra("user");
+
+        // Weryfikacja tokenu
+        if (jwtToken != null && !jwtToken.isEmpty()) {
+            Toast.makeText(this, "Magazyn", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent(this, login.class);
+            startActivity(intent);
+            finish(); // Dodaj finish, aby zakończyć bieżącą aktywność, jeśli nie ma tokenu
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.change) {
+            Intent intent = new Intent(this, password.class);
+            intent.putExtra("user", jwtToken);
+            startActivity(intent);
+            return true;
+        }else if(item.getItemId() == R.id.main){
+            Intent intent = new Intent(this, main.class);
+            intent.putExtra("user", jwtToken);
+            startActivity(intent);
+            return true;
+        }
+        else if (item.getItemId() == R.id.logout) {
+            performLogout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void performLogout() {
+        // Tutaj umieść kod do wykonania wylogowania
+        // Na przykład, usuń token uwierzytelniający, czyśc pamięć podręczną itp.
+
+        // Przykład: Powrót do ekranu logowania
+        getIntent().removeExtra("user");
+        jwtToken="";
+        Intent intent = new Intent(this, login.class);
+        startActivity(intent);
+        finish(); // Opcjonalnie zamyka aktualną aktywność, aby użytkownik nie mógł wrócić przyciskiem "Wstecz"
+    }
+    }
